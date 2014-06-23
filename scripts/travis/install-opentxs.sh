513b25fe064f7d8d3b97c8721db8c61af3eca15d
Locutus <locutus@localhost.localdomain>
2014-06-23 05:39:03 -0400

    updated .travis.yml to back out of build dir before trying to build tarball, 1-line change

--- scripts/travis/install-opentxs.sh
+++ scripts/travis/install-opentxs.sh
@@ -13,7 +13,7 @@ fi
 
 aws_url="https://s3-eu-west-1.amazonaws.com"
 
-version="0.92.d"
+version="0.93"
 
 package="opentxs-${version}-${os}-${compiler}.tar.gz"
 url=`echo "${aws_url}/monetas-builds/monetas/opentxs/develop/${package}" | sed 's/+/%2B/g'`
