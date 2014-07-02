7ad72b4477048b9daf4046ff8aca8c5ab563f376
Filip Gospodinov <filip@monetas.net>
2014-07-02 14:12:49 +0200

    travis: add clang-format support

---
+++ scripts/travis/clang-format-wrapper.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+clang_format_bin=clang-format
+if command -v clang-format-3.4 >/dev/null; then
+    clang_format_bin=clang-format-3.4
+fi
+
+files="$@"
+
+for file in $files; do
+    $clang_format_bin -style=file "$file" | \
+        sed 's/^\( *\)} else/\1}\n\1else/g' > "$file.clangformat"
+    git diff --no-index "$file" "$file.clangformat" | cat
+    rm "$file.clangformat"
+done
