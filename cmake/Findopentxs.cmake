1460f90ed976e1677b6a2a038826a59a93d3de32
Justus Ranvier <justus@stashcrypto.com>
2016-11-01 22:51:02 -0500

    Update for changes to opentxs

---
+++ cmake/Findopentxs.cmake
@@ -0,0 +1,18 @@
+include(FindPkgConfig)
+  PKG_CHECK_MODULES(PC_OPENTXS "libopentxs")
+
+  find_path(
+      OPENTXS_INCLUDE_DIRS
+      NAMES opentxs
+      HINTS ${PC_OPENTXS_INCLUDE_DIRS}
+  )
+
+  find_library(
+      OPENTXS_LIBRARIES
+      NAMES libopentxs.so libopentxs.a libopentxs.dynlib
+      HINTS ${PC_OPENTXS_LIBRARY_DIRS}
+  )
+
+  include(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(OPENTXS DEFAULT_MSG OPENTXS_LIBRARIES OPENTXS_INCLUDE_DIRS)
+  mark_as_advanced(OPENTXS_LIBRARIES OPENTXS_INCLUDE_DIRS)
