9c6dc256856f486e4aa3932a8c2b1e8ffaf611a6
Justus Ranvier <justus@opentransactions.org>
2024-02-02 07:18:56 -0600

    update copyright headers

--- tools/cmake/opentxs-notary-print-build-details.cmake
+++ tools/cmake/opentxs-notary-print-build-details.cmake
@@ -1,7 +1,7 @@
-# Copyright (c) 2010-2023 The Open-Transactions developers This Source Code Form
-# is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
-# the MPL was not distributed with this file, You can obtain one at
-# http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2010-2024 The Open-Transactions developers
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 macro(opentxs_notary_print_build_details)
   include(otcommon-print-build-details)
